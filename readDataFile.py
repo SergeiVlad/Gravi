383c5803623b007025217727d067f4e589269e47
Sergey Vladm <sv97095@gmail.com>
2017-10-14 19:26:56 +0300

    new commit for add readDataFile.py again

---
+++ readDataFile.py
@@ -0,0 +1,16 @@
+""" readDataFile module read data after scaning file by scanFile module """
+
+def read(D):
+
+	if type(D) == dict:
+
+		print('OK')
+
+	else:
+
+		print('Ne Ok')
+	
+
+
+
+	return('Ok1')
