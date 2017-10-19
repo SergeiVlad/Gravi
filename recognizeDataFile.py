bac635db4584c396f8116eddf29f37e9fc6a15a7
Sergei G <dneprfk@gmail.com>
2017-10-19 15:49:34 +0300

    recognizeDataFile module begin

---
+++ recognizeDataFile.py
@@ -0,0 +1,19 @@
+""" This module recognizing the structure of data in file 
+
+This module get and use information from text file 'DataFileTypes'.
+In this file gathered all known formats of work the Data Files. 
+"""
+
+def recognize(D):
+	""" Recognize current data file by dictionary D that return from scanFile
+	
+		Return list with field names of the data in current file 
+	"""
+
+	return DataList
+
+"""
+Grom_1s_with_TM
+F = {1: "Second", 2: "Gx", 3: "Gy", 4: "Gz", 5: "Ax", 6: "Ay", 7: "Az"}
+
+"""
