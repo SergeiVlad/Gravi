622b7b1fd174e6d0491f454b5f08dd28e335ff4d
Sergey Vladm <sv97095@gmail.com>
2017-10-20 00:28:50 +0300

    projecting read

--- recognizeDataFile.py
+++ recognizeDataFile.py
@@ -1,19 +1,30 @@
 """ This module recognizing the structure of data in file 
 
-This module get and use information from text file 'DataFileTypes'.
+This module get and use information from text file 'DataFileTypes.txt'.
 In this file gathered all known formats of work the Data Files. 
 """
 
 def recognize(D):
+
 	""" Recognize current data file by dictionary D that return from scanFile
 	
 		Return list with field names of the data in current file 
 	"""
 
+	DataList = ''
+
+	# recognizing existing data type
+	d# ------------------------------------------------------
+	ls = ['int', 'int', 'float', 'float', 'float', 'float', 'float', 'float', 'int']
+	if D['types'] == ls:
+		data_name = 'grom_TM01'
+
+
+	
+
+
+
+
+
+
 	return DataList
-
-"""
-Grom_1s_with_TM
-F = {1: "Second", 2: "Gx", 3: "Gy", 4: "Gz", 5: "Ax", 6: "Ay", 7: "Az"}
-
-"""
