90a983368056adbdaee3e61bdf3fb25617da8693
Sergei G <dneprfk@gmail.com>
2018-02-05 16:39:36 +0200

    edit main.py

--- main.py
+++ main.py
@@ -1,34 +1,61 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog,
-                             QPushButton, QDesktopWidget, QTabWidget, QVBoxLayout)
+                             QPushButton, QDesktopWidget, QTabWidget, QVBoxLayout, 
+                             QMainWindow, QListWidget)
 from PyQt5.QtGui import QIcon
  
-class App(QWidget):
+class App(QMainWindow):
  
     def __init__(self):
         super().__init__()
         self.title = 'Gravi'
         self.resize(250, 400)
-        self.initUI()
- 
-    def initUI(self):
         self.setWindowTitle(self.title)
-        self.setWindowIcon(QIcon('icon.png'))
+        self.setWindowIcon(QIcon('icon2.png'))
         self.center()
 
-        btn = QPushButton('Open',self)
-        btn.move(50, 50)
-        self.openFileNameDialog()
- 
+        self.tab_widget = MytableWidget(self)
+        self.setCentralWidget(self.tab_widget)
+
         self.show()
 
     def center(self):
-
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+class MytableWidget(QWidget):
+    """docstring for MytableWidget"""
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)        
+
+        #Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tabs.resize(300, 200)
+
+        # Add tabs
+        self.tabs.addTab(self.tab1, "Files")
+        self.tabs.addTab(self.tab2, "Dir")
+
+        # Create tab 1
+        self.tab1.layout = QVBoxLayout(self)
+        self.btn1 = QPushButton("Open")
+        self.lst1 = QListWidget()
+        self.btn1.clicked.connect(self.openFileNameDialog)
+        self.tab1.layout.addWidget(self.lst1)
+        self.tab1.layout.addWidget(self.btn1)
+        self.tab1.setLayout(self.tab1.layout)
+
+        # Create tab 2
+        
+
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
 
     def openFileNameDialog(self):    
         options = QFileDialog.Options()
